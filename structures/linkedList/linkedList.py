c08da28fc03aff61a71c57a135856593c6e61533
João Bispo <joaobispo2077@gmail.com>
2022-03-16 19:15:09 -0300

    feat: add same linked list methods in python

--- structures/linkedList/linkedList.py
+++ structures/linkedList/linkedList.py
@@ -6,6 +6,19 @@ class LinkedList:
         self.head = None
         self._size
 
+    def _get_node(self, target_index):
+        pointer = self.head
+        for index in range(target_index):
+            if pointer:
+                pointer = pointer.next
+            else:
+                return None
+
+        if pointer:
+            return pointer
+        else:
+            return None
+
     def __len__(self):
         return self._size
 
@@ -45,3 +58,57 @@ class LinkedList:
             return data
         else:
             return None
+
+    def search(self, element):
+        pointer = self.head
+        while(pointer):
+            if pointer.data == element:
+                return pointer.data
+            pointer = pointer.next
+        return None
+
+    def insert(self, target_index, element):
+        node = Node(element)
+
+        if target_index == 0:
+            node.next = self.head
+            self.head = node
+        else:
+            pointer = self._get_node(target_index - 1)
+            node.next = pointer.next
+            pointer.next = node
+
+        self._size = self._size + 1
+
+    def remove(self, element):
+        if self.head == None:
+            return None
+        elif self.head.data == element:
+            self.head = self.head.next
+            self._size = self._size - 1
+            return True
+        else:
+            previous_pointer = self.head
+            pointer = self.head.next
+
+            while(pointer):
+                if pointer.data == element:
+                    previous_pointer.next = pointer.next
+                    self._size = self._size - 1
+                    return True
+
+                previous_pointer = pointer
+                pointer = pointer.next
+
+            return False
+
+    def __repr__(self):
+        text_list = ""
+        pointer = self.head
+        while(pointer):
+            text_list = text_list + str(pointer.data) + "->"
+            pointer = pointer.next
+        return text_list
+
+    def __str__(self):
+        return self.__repr__()
