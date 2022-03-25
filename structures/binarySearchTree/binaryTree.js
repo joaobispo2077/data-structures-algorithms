96fd6505526bd97d8b017bcd408bf8fea0764dad
João Bispo <joaobispo2077@gmail.com>
2022-03-25 14:50:44 -0300

    refactor: extract binary tree from index.js to binaryTree.js

---
+++ structures/binarySearchTree/binaryTree.js
@@ -0,0 +1,103 @@
+const { Queue } = require("./queue");
+
+class BinaryTree {
+  constructor(data = null) {
+    if (data) {
+      const node = new Node(data);
+      this.root = node;
+    } else {
+      this.root = data;
+    }
+  }
+
+  show() {
+    console.log(this.root);
+  }
+
+  // in order traversal
+  inOrderTraversal(treeNode = null) {
+    if (treeNode === null) {
+      treeNode = this.root;
+    }
+
+    if (treeNode.left) {
+      this.inOrderTraversal(treeNode.left);
+    }
+
+    console.log(treeNode.data);
+
+    if (treeNode.right) {
+      this.inOrderTraversal(treeNode.right);
+    }
+
+  }
+
+  postOrderTraversal(treeNode = null) {
+    if (treeNode === null) {
+      treeNode = this.root;
+    }
+
+    if (treeNode.left) {
+      this.postOrderTraversal(treeNode.left);
+    }
+
+    if (treeNode.right) {
+      this.postOrderTraversal(treeNode.right);
+    }
+
+    process.stdout.write(treeNode.data);
+  }
+
+  levelOrderTraversal(treeNode = DEFAULT_NODE) {
+    if (treeNode === DEFAULT_NODE) {
+      treeNode = this.root;
+    }
+
+    const queue = new Queue();
+    queue.enqueue(treeNode);
+
+    while (queue.length > 0) {
+      const node = queue.dequeue();
+
+      if (node.left) {
+        queue.enqueue(node.left);
+      }
+
+      if (node.right) {
+        queue.enqueue(node.right);
+      }
+
+      if (node.data) {
+        process.stdout.write(` ${node.data} `);
+      }
+    }
+
+
+  }
+
+  getHeight(treeNode = null) {
+    if (treeNode === null) {
+      treeNode = this.root;
+    }
+
+    let leftHeight = 0;
+    let rightHeight = 0;
+
+    if (treeNode.left) {
+      leftHeight = this.getHeight(treeNode.left);
+    }
+
+    if (treeNode.right) {
+      rightHeight = this.getHeight(treeNode.right);
+    }
+
+    if (rightHeight > leftHeight) {
+      return rightHeight + 1;
+    }
+
+    return leftHeight + 1;
+  }
+}
+
+
+module.exports = { BinaryTree };
--- structures/binarySearchTree/index.js
+++ structures/binarySearchTree/index.js
@@ -1,4 +1,4 @@
-const { Queue } = require("./queue");
+const { BinaryTree } = require("./binaryTree");
 
 const DEFAULT_NODE = "DEFAULT_NODE";
 class Node {
@@ -6,105 +6,6 @@ class Node {
     this.data = data;
     this.left = null;
     this.right = null;
-  }
-}
-
-class BinaryTree {
-  constructor(data = null) {
-    if (data) {
-      const node = new Node(data);
-      this.root = node;
-    } else {
-      this.root = data;
-    }
-  }
-
-  show() {
-    console.log(this.root);
-  }
-
-  // in order traversal
-  inOrderTraversal(treeNode = null) {
-    if (treeNode === null) {
-      treeNode = this.root;
-    }
-
-    if (treeNode.left) {
-      this.inOrderTraversal(treeNode.left);
-    }
-
-    console.log(treeNode.data);
-
-    if (treeNode.right) {
-      this.inOrderTraversal(treeNode.right);
-    }
-
-  }
-
-  postOrderTraversal(treeNode = null) {
-    if (treeNode === null) {
-      treeNode = this.root;
-    }
-
-    if (treeNode.left) {
-      this.postOrderTraversal(treeNode.left);
-    }
-
-    if (treeNode.right) {
-      this.postOrderTraversal(treeNode.right);
-    }
-
-    process.stdout.write(treeNode.data);
-  }
-
-  levelOrderTraversal(treeNode = DEFAULT_NODE) {
-    if (treeNode === DEFAULT_NODE) {
-      treeNode = this.root;
-    }
-
-    const queue = new Queue();
-    queue.enqueue(treeNode);
-
-    while (queue.length > 0) {
-      const node = queue.dequeue();
-
-      if (node.left) {
-        queue.enqueue(node.left);
-      }
-
-      if (node.right) {
-        queue.enqueue(node.right);
-      }
-
-      if (node.data) {
-        process.stdout.write(` ${node.data} `);
-      }
-    }
-
-
-  }
-
-  getHeight(treeNode = null) {
-    if (treeNode === null) {
-      treeNode = this.root;
-    }
-
-    let leftHeight = 0;
-    let rightHeight = 0;
-
-    if (treeNode.left) {
-      leftHeight = this.getHeight(treeNode.left);
-    }
-
-    if (treeNode.right) {
-      rightHeight = this.getHeight(treeNode.right);
-    }
-
-    if (rightHeight > leftHeight) {
-      return rightHeight + 1;
-    }
-
-    return leftHeight + 1;
   }
 }
 
